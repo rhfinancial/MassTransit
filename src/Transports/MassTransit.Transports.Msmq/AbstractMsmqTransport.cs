9d7645ab21b534a1da8b0a326f88c19f636bd333
gkutka <gkutka@users.noreply.github.com>
2015-01-09 13:11:03 -0500

    Added message extension data check for null

--- src/Transports/MassTransit.Transports.Msmq/AbstractMsmqTransport.cs
+++ src/Transports/MassTransit.Transports.Msmq/AbstractMsmqTransport.cs
@@ -116,7 +116,7 @@ namespace MassTransit.Transports.Msmq
 			        // Get the tag from current.Extension
                     var data = current.Extension;
 
-			        if (data.Length >= TraceTagLength)
+                    if (data != null && data.Length >= TraceTagLength)
                     {
                         var offset = data.Length - TraceTagLength;
                         var tagOffset = offset + TracetagSize;
